897d4e90f5dc80148b8e16aa2e613d5124bc00d4
Erick <erick.nahuelpaiva.001@gmail.com>
2023-03-31 21:10:40 -0300

    ejemplo de variables terminado

---
+++ README.md
@@ -0,0 +1,10 @@
+# Informacion
+
+- nombre de mi mascota
+- dni personal
+- confirmacion para salir el finde
+- precio de alguna compra
+- nombre del centro
+- contraseña personal (1!"·_.-,asf_false)
+- mensaje de la nota final en un examen
+- inicial de un nombre
---
+++ variables.js
@@ -0,0 +1,17 @@
+let nombreMascota = "toffee";//string - palabra
+let numeroDni = 100000;//entero - int
+let salidaConfirmada = false;//boolean - si = true y no = false
+let precioCompra = 12312.4;//float - decimal . para diferenciar decimales
+let nombreCentroFormacion = "CFP 5";//string - palabra
+let clavePersonal = `1!"·_.-,asf_false`;
+let msjNotaFinal = "Hola ??? felicidades por sacar la nota maxima del modulo, nota final: 10";
+let inicialNombre = 'E';//char - caracter
+
+/* let variablePalabra = "laskf123!"4,_.,""; */
+let variablePalabra = 'laskf123!"4,_.,"';
+let variablePalabra2 = `laskf123!"4,_.,"`;
+
+//  string - palabra usamos ""
+// char - caracter ''
+// en js pueden usar cualquiera de los 3 y de preferencia ``
+// `` permite salto de linea
